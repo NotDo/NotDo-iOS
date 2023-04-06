128726e197883a52253114b6409b1a1dc75db28f
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-07 02:52:29 +0900

    :wrench: BaseViewController

---
+++ Projects/Feature/BaseFeature/Sources/Base/BaseViewController.swift
@@ -0,0 +1,99 @@
+import Combine
+import DesignSystem
+import UIKit
+
+open class BaseViewController<Store: BaseStore>: UIViewController, BoundsProviable {
+
+    // MARK: - Properties
+
+    private let viewDidLoadSubject = PassthroughSubject<Void, Never>()
+    private let viewWillAppearSubject = PassthroughSubject<Void, Never>()
+    private let viewDidAppearSubject = PassthroughSubject<Void, Never>()
+    private let viewWillDisappearSubject = PassthroughSubject<Void, Never>()
+    private let viewDidDisappearSubject = PassthroughSubject<Void, Never>()
+    public let store: Store
+    public var bag = Set<AnyCancellable>()
+
+    // MARK: - Init
+
+    public init(store: Store) {
+        self.store = store
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - LifeCycle
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .notDo(.main(.white))
+        addView()
+        setLayout()
+        configureViewController()
+        configureNavigation()
+        bindAction()
+        bind()
+        viewDidLoadSubject.send(())
+    }
+
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewWillAppearSubject.send(())
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewDidAppearSubject.send(())
+    }
+
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewWillDisappearSubject.send(())
+    }
+
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        viewDidDisappearSubject.send(())
+    }
+
+    // MARK: - UI
+
+    open func addView() {}
+
+    open func setLayout() {}
+
+    open func configureViewController() {}
+
+    open func configureNavigation() {}
+
+    open func bindAction() {}
+
+    open func bind() {}
+}
+
+// MARK: - LifeCyclePublishable
+
+extension BaseViewController: LifeCyclePublishable {
+    public var viewDidLoadPublisher: AnyPublisher<Void, Never> {
+        viewDidLoadSubject.eraseToAnyPublisher()
+    }
+
+    public var viewWillAppearPublisher: AnyPublisher<Void, Never> {
+        viewWillAppearSubject.eraseToAnyPublisher()
+    }
+
+    public var viewDidAppearPublisher: AnyPublisher<Void, Never> {
+        viewDidAppearSubject.eraseToAnyPublisher()
+    }
+
+    public var viewWillDisappearPublisher: AnyPublisher<Void, Never> {
+        viewWillDisappearSubject.eraseToAnyPublisher()
+    }
+
+    public var viewDidDisappearPublisher: AnyPublisher<Void, Never> {
+        viewDidDisappearSubject.eraseToAnyPublisher()
+    }
+}
