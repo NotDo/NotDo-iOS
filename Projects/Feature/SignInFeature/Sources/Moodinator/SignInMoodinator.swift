2a24a2c56f821913cf853b761d0a2d88ab66baac
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-14 15:11:02 +0900

    :wrench: Moodinator setting

--- Projects/Feature/SignInFeature/Sources/Moodinator/SignInMoodinator.swift
+++ Projects/Feature/SignInFeature/Sources/Moodinator/SignInMoodinator.swift
@@ -1,9 +1,33 @@
-//
-//  SignInMoodinator.swift
-//  SignInFeatureInterface
-//
-//  Created by sunghun on 2023/04/14.
-//  Copyright © 2023 NotDo. All rights reserved.
-//
+import DesignSystem
+import Moordinator
+import UIKit
+import SignInFeatureInterface
 
-import Foundation
+final class SignInMoordinator: Moordinator {
+    private let rootVC = UINavigationController()
+    let router: any Router
+    private let signInViewController: SignInViewController
+
+    init(
+        router: SignInRouter,
+        signInViewController: SignInViewController
+    ) {
+        self.router = router
+        self.signInViewController = signInViewController
+    }
+
+    var root: Presentable {
+        rootVC
+    }
+
+    func route(to path: RoutePath) -> MoordinatorContributors {
+        guard let path = path as? SignInRoutePath else { return .none }
+        switch path {
+        case .signIn:
+            rootVC.setViewControllers([signInViewController], animated: true)
+        default:
+            return .none
+        }
+        return .none
+    }
+}
