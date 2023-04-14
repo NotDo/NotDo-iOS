b7222f3ae476da89cc8dddcf32e674adee18ee61
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-14 15:09:49 +0900

    :wrench: SignInViewController 생성

--- Projects/Feature/SignInFeature/Sources/Scene/SignInViewController.swift
+++ Projects/Feature/SignInFeature/Sources/Scene/SignInViewController.swift
@@ -1,9 +1,9 @@
-//
-//  SignInViewController.swift
-//  SignInFeatureInterface
-//
-//  Created by sunghun on 2023/04/14.
-//  Copyright © 2023 NotDo. All rights reserved.
-//
+import UIKit
+import DesignSystem
+import BaseFeature
+import SnapKit
+import Then
 
-import Foundation
+final class SignInViewController: BaseViewController<SignInStore> {
+
+}
