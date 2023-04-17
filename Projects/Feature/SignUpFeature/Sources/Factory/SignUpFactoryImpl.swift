6bbae32864fb3c96b38b3d4a749f8c179320ca6b
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-18 01:39:15 +0900

    :wrench: SignUpFactoryImpl 세팅

--- Projects/Feature/SignUpFeature/Sources/Factory/SignUpFactoryImpl.swift
+++ Projects/Feature/SignUpFeature/Sources/Factory/SignUpFactoryImpl.swift
@@ -1,1 +1,16 @@
-import
+import Moordinator
+import SignUpFeatureInterface
+
+struct SignUpFactoryImpl: SignUpFactory {
+    func makeMoordinator() -> Moordinator {
+        let signUpRouter = SignUpRouter()
+        let signUpStore = SignUpStore(
+            router: signUpRouter
+        )
+        let signUpViewController = SignUpViewController(store: signUpStore)
+        return SignUpMoordinator(
+            router: signUpRouter,
+            signUpViewController: signUpViewController
+        )
+    }
+}
