adca65dffc8644de33e29c485a7227b6d6db3467
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-18 01:37:32 +0900

    :wrench: SignUpMoodinator 세팅

--- Projects/Feature/SignUpFeature/Sources/Moodinator/SignUpMoodinator.swift
+++ Projects/Feature/SignUpFeature/Sources/Moodinator/SignUpMoodinator.swift
@@ -1,9 +1,33 @@
-//
-//  SignUpMoodinator.swift
-//  SignUpFeatureInterface
-//
-//  Created by sunghun on 2023/04/18.
-//  Copyright © 2023 NotDo. All rights reserved.
-//
+import DesignSystem
+import Moordinator
+import UIKit
+import SignUpFeatureInterface
 
-import Foundation
+final class SignUpMoordinator: Moordinator {
+    private let rootVC = UINavigationController()
+    let router: any Router
+    private let signUpViewController: SignUpViewController
+
+    init(
+        router: SignUpRouter,
+        signUpViewController: SignUpViewController
+    ) {
+        self.router = router
+        self.signUpViewController = signUpViewController
+    }
+
+    var root: Presentable {
+        rootVC
+    }
+
+    func route(to path: RoutePath) -> MoordinatorContributors {
+        guard let path = path as? SignUpRoutePath else { return .none }
+        switch path {
+        case .signUp:
+            rootVC.pushViewController(signUpViewController, animated: true)
+        default:
+            return .none
+        }
+        return .none
+    }
+}
