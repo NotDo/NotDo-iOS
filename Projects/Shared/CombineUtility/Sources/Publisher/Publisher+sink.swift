4c3c2b87445f4cdd285f6497aecf2ecb1b79aa2c
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-18 03:03:40 +0900

    :wrench: Store를 손쉽게 가져오기 위한 extension

--- Projects/Shared/CombineUtility/Sources/Publisher/Publisher+sink.swift
+++ Projects/Shared/CombineUtility/Sources/Publisher/Publisher+sink.swift
@@ -1,9 +1,17 @@
-//
-//  Publisher+sink.swift
-//  CombineUtility
-//
-//  Created by sunghun on 2023/04/18.
-//  Copyright © 2023 NotDo. All rights reserved.
-//
+import Combine
 
-import Foundation
+public extension Publisher {
+    func sink<Object: AnyObject>(
+        with object: Object,
+        receiveCompletion: @escaping ((Object, Subscribers.Completion<Failure>) -> Void) = { _, _ in },
+        receiveValue: @escaping ((Object, Output) -> Void) = { _, _ in }
+    ) -> AnyCancellable {
+        self.sink { [weak object] completion in
+            guard let object = object else { return }
+            receiveCompletion(object, completion)
+        } receiveValue: { [weak object] value in
+            guard let object = object else { return }
+            receiveValue(object, value)
+        }
+    }
+}
