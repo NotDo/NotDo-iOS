f8af29117fade027e0458045c4974651b35b18ac
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-04-18 15:58:55 +0900

    :zap: Action 추가

--- Projects/Feature/SignUpFeature/Sources/Scene/SignUpStore.swift
+++ Projects/Feature/SignUpFeature/Sources/Scene/SignUpStore.swift
@@ -2,6 +2,7 @@ import BaseFeature
 import Combine
 import DesignSystem
 import Moordinator
+import SignUpFeatureInterface
 
 final class SignUpStore: BaseStore, RouterProvidable {
     let router: any Router
@@ -14,12 +15,26 @@ final class SignUpStore: BaseStore, RouterProvidable {
     }
 
     struct State: Equatable {
+        var isCheck: Bool = false
     }
     enum Action: Equatable {
+        case checkButtonDidTap
+        case seeMoreButtonDidTap
     }
 
     let stateSubject = CurrentValueSubject<State, Never>(State())
 
     func process(_ action: Action) {
+        let currentState = stateSubject.value
+        var newState = currentState
+
+        switch action {
+        case .checkButtonDidTap:
+            newState.isCheck.toggle()
+        case .seeMoreButtonDidTap:
+            router.route.send(SignUpRoutePath.findPassword)
+        }
+
+        stateSubject.send(newState)
     }
 }
--- Projects/Feature/SignUpFeature/Sources/Scene/SignUpViewController.swift
+++ Projects/Feature/SignUpFeature/Sources/Scene/SignUpViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import DesignSystem
 import BaseFeature
+import GlobalThirdPartyLibrary
+import CombineUtility
 import SignUpFeatureInterface
 
 final class SignUpViewController: BaseViewController<SignUpStore> {
@@ -19,7 +21,14 @@ final class SignUpViewController: BaseViewController<SignUpStore> {
         $0.font = .notDo(.caption2Medium)
     }
 
-    private let checkBox = NotDoCheckBox()
+    private let checkBox = UIButton().then {
+        $0.backgroundColor = .clear
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.notDo(.gray(.gray400)).cgColor
+        $0.layer.cornerRadius = 7
+        $0.setImage(.bigCheckBox, for: .normal)
+        $0.imageView?.layer.transform = CATransform3DMakeScale(1.36, 1.36, 1.36)
+    }
 
     private let privacyLabel = UILabel().then {
         $0.text = "개인정보 3자 제공 동의"
@@ -33,7 +42,7 @@ final class SignUpViewController: BaseViewController<SignUpStore> {
     }
 
     private let nextButton = NotDoCTAButton(text: "다음").then {
-        $0.isEnabled = true
+        $0.isEnabled = false
     }
 
     override func addView() {
@@ -67,11 +76,12 @@ final class SignUpViewController: BaseViewController<SignUpStore> {
         checkBox.snp.makeConstraints {
             $0.leading.equalTo(termsLabel.snp.leading)
             $0.top.equalTo(termsLabel.snp.bottom).offset(10)
+            $0.size.equalTo(24)
         }
 
         privacyLabel.snp.makeConstraints {
             $0.leading.equalTo(checkBox.snp.trailing).offset(10)
-            $0.top.equalTo(checkBox.snp.top)
+            $0.centerY.equalTo(checkBox.snp.centerY)
         }
 
         seeMoreButton.snp.makeConstraints {
@@ -83,5 +93,29 @@ final class SignUpViewController: BaseViewController<SignUpStore> {
             $0.bottom.equalTo(view.snp.bottom).inset(48)
             $0.leading.trailing.equalToSuperview().inset(16)
         }
+    }
+
+    override func bindAction() {
+        let currentStore = store.state.share()
+
+        checkBox.tapPublisher
+            .sink(with: store, receiveValue: { store, _ in
+                store.process(.checkButtonDidTap)
+            })
+            .store(in: &bag)
+
+        seeMoreButton.tapPublisher
+            .sink(with: store, receiveValue: { store, _ in
+                store.process(.seeMoreButtonDidTap)
+            })
+            .store(in: &bag)
+
+        currentStore
+            .sink(with: self, receiveValue: { owner, store in
+                owner.checkBox.setImage(store.isCheck ? UIImage.checkBox : UIImage.init(), for: .normal)
+                owner.checkBox.layer.borderWidth = store.isCheck ? 0 : 1
+                owner.nextButton.isEnabled = store.isCheck
+            })
+            .store(in: &bag)
     }
 }
